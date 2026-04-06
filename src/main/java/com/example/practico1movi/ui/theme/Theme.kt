05150ae11ba47c041bee4b773318b342ab864050
hendykg <96022246+hendykg@users.noreply.github.com>
2026-04-06 09:28:28 -0400

    Update Theme.kt

--- src/main/java/com/example/practico1movi/ui/theme/Theme.kt
+++ src/main/java/com/example/practico1movi/ui/theme/Theme.kt
@@ -34,7 +34,7 @@ private val LightColorScheme = lightColorScheme(
 )
 
 @Composable
-fun Practico2Theme(
+fun PracticomoviTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
